22431ee472e6b0d7accae8c2e7e8bd8f4df15a75
unknown <1212962259@TSNEM3086.2t.local>
2016-01-19 12:29:25 +0000

    Uploading work done in class

--- FreshAir_Triggers.sql
+++ FreshAir_Triggers.sql
@@ -1,10 +1,27 @@
 USE `1212962259_freshair_international`;
 
 DELIMITER $$
-
-CREATE TRIGGER before_flight_insert
-BEFORE INSERT ON flights
+DROP TRIGGER IF EXISTS after_flight_insert $$
+CREATE TRIGGER after_flight_insert
+AFTER INSERT ON flights
 FOR EACH ROW
-BEGIN
+  BEGIN
+    #No idea how to stop the insert in the slightest
+  END $$
+DELIMITER ;
 
-END $$
+-- Functions(vonandi)
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS count_seats $$
+CREATE FUNCTION count_seats(aircraft_id CHAR(6))
+  RETURNS INT
+  BEGIN
+    DECLARE seat_count INT;
+    SELECT count(seatID) into seat_count
+    FROM aircraftSeats
+    WHERE aircraftSeats.aircraftID = aircraft_id;
+    return seat_count;
+
+  END $$
+DELIMITER ;
